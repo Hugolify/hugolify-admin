f560a5813f37629cbc73d3f30b5e261937e11852
sebousan <sebousan@gmail.com>
2025-03-26 16:07:17 +0100

    tinacms wip

--- layouts/partials/admin/cms/tinacms/config.js
+++ layouts/partials/admin/cms/tinacms/config.js
@@ -1,17 +1,29 @@
 {{- $collections := partialCached "admin/collections/index.yml" . -}}
 {{- $sliceCompatibity := slice 
+  (dict "regex" true "path: '[^']*'," "") 
   (dict "folder:" "path:") 
+  (dict "widget: 'color'" "widget: 'string'") 
   (dict "widget: 'relation'" "widget: 'reference'") 
   (dict "widget: 'markdown'" "widget: 'rich-text'") 
   (dict "widget: 'text'" "widget: 'string'") 
-  (dict "widget: 'hidden'" "widget: 'string', hidden: true") 
-  (dict "widget: 'select'" "widget: 'string', list: true") 
+  (dict "widget: 'hidden'" "widget: 'string', ui: { hidden: true }") 
+  (dict "widget: 'select'" "widget: 'string', list: true, ui: { component: 'select' }") 
+  (dict "name: 'pdf'" "widget: 'pdf', media: { accept: 'application/pdf' }") 
+  (dict "name: 'mp4'" "widget: 'mp4', media: { accept: 'video/mp4' }") 
+  (dict "name: 'webm'" "widget: 'webm', media: { accept: 'video/webm' }") 
+  (dict "widget: 'file'" "widget: 'image'") 
+  (dict "types:" "templates:") 
   (dict "widget: 'list'" "widget: 'object', list: true") 
   (dict "widget:" "type:") 
   -}}
 {{- range $sliceCompatibity }}
+  {{- $regex := .regex | default false -}}
   {{- range $key, $value := . }}
+    {{- if $regex -}}
+    {{- $collections = replaceRE $key $value $collections -}}
+    {{- else -}}
     {{- $collections = replace $collections $key $value -}}
+    {{- end -}}
   {{- end -}}
 {{- end -}}
 
@@ -27,7 +39,7 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "/assets/images/uploads",
+      mediaRoot: "assets/images/uploads",
       publicFolder: "public",
     },
   },
