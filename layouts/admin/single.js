4dbba09cf861f3b64aef1346632e4eb016a0fa98
sebousan <sebousan@gmail.com>
2025-10-23 12:07:46 +0200

    fix indexation

--- layouts/admin/single.js
+++ layouts/admin/single.js
@@ -1,5 +1,5 @@
 {{- $cms := site.Params.admin.cms | default "tinacms" -}}
 {{- $file := printf "admin/cms/%s/config.js" $cms -}}
 {{- if templates.Exists ( printf "partials/%s" $file ) }}
-  {{ partialCached $file . }}
+  {{- partialCached $file . }}
 {{ end -}}
