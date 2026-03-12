988634229b2f9be2e75326b6a594e541122e50fb
sebousan <sebousan@gmail.com>
2026-03-12 09:24:59 +0100

    doc: add comment to Netlify shortcodes

--- layouts/partials/admin/cms/netlifycms/shortcodes.js
+++ layouts/partials/admin/cms/netlifycms/shortcodes.js
@@ -1,1 +1,5 @@
+{{/*
+  Sveltia CMS uses the same shortcode registration as Decap CMS
+  (CMS.registerEditorComponent calls are identical between both).
+*/}}
 {{ partialCached "admin/cms/decapcms/shortcodes.js" . }}
