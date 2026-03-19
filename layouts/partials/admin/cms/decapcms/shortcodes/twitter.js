2a0119ce9829ab07b6e0abe5dc126283e282c72b
sebousan <sebousan@gmail.com>
2026-03-19 19:59:08 +0100

    refactor: update Twitter shortcode from 'tweet' to 'x' and its label to 'Twitter / X'.

--- layouts/partials/admin/cms/decapcms/shortcodes/twitter.js
+++ layouts/partials/admin/cms/decapcms/shortcodes/twitter.js
@@ -1,18 +1,18 @@
 {{/*
   Custom implementation — cannot use _register.js because:
-  - The Hugo shortcode name is "tweet" but the CMS component id is "twitter".
+  - The Hugo shortcode name is "x" but the CMS component id is "twitter/x".
   - _register.js uses the same name for both the pattern and the component id.
 */}}
 {{- $fields := partialCached "admin/shortcodes/fields/twitter.html" . }}
 CMS.registerEditorComponent({
   id: 'twitter',
-  label: '{{ i18n "admin.shortcodes.twitter.label" | default "Twitter" }}',
+  label: '{{ i18n "admin.shortcodes.twitter.label" | default "Twitter / X" }}',
   fields: [
     {{- range $fields }}
     {{ partialCached (print "admin/fields/" . ".yml") . . | safeHTML }},
     {{- end }}
   ],
-  pattern: /{{`{{< tweet user="(.*?)" id="(.*)" >}}` | safeHTML }}/,
+  pattern: /{{`{{< x user="(.*?)" id="(.*)" >}}` | safeHTML }}/,
   fromBlock: function (match) {
     return {
       user: match[1],
@@ -22,9 +22,9 @@ CMS.registerEditorComponent({
   toBlock: function (obj) {
     const user = obj.user || '';
     const id = obj.id || '';
-    return '{{ `{{<` | safeHTML }} tweet user="' + user + '" id="' + id + '" {{ `>}}` | safeHTML }}';
+    return '{{ `{{<` | safeHTML }} x user="' + user + '" id="' + id + '" {{ `>}}` | safeHTML }}';
   },
   toPreview: function (obj) {
-    return '{{`{{< tweet user="${obj.user}" id="${obj.id}" >}}`}}';
+    return '{{`{{< x user="${obj.user}" id="${obj.id}" >}}`}}';
   }
 });
